666693a28eb3d721664d3a61a07b42b89f787290
Ankur Papneja <ankurpapneja4@gmail.com>
2024-06-12 17:07:49 +0530

    Hello World - added

---
+++ src/main/groovy/com/github/ankurpapneja4/HelloGroovy.groovy
@@ -0,0 +1,5 @@
+package com.github.ankurpapneja4
+
+// Print Hello World
+println "Hello World"
+
---
+++ src/main/groovy/com/github/ankurpapneja4/PrintFormatted.groovy
@@ -0,0 +1,20 @@
+package com.github.ankurpapneja4;
+
+// Integers
+def n = 109985000
+
+printf "%d", n  // 109985000
+printf "%n"     // New Line Character
+printf "%,d",n  // 109,985,000
+
+
+// Binary
+def b = 0xA4
+
+printf "%d %n",  b // 164
+printf "%h %n",  b // a4
+printf "%H %n",  b // A4
+printf "%x %n",  b // a4
+printf "%X %n",  b // A4
+printf "%#X %n", b // 0xA4
+
