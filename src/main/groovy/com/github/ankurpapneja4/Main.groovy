a9c950301b5772bc27a5c1f02dbfd098b21ef02a
Ankur Papneja <ankurpapneja4@gmail.com>
2024-06-12 13:29:07 +0530

    Maven-Groovy project setup

---
+++ src/main/groovy/com/github/ankurpapneja4/Main.groovy
@@ -0,0 +1,5 @@
+package com.github.ankurpapneja4
+
+def sum = Calculator.sum 3,4
+println sum
+
---
+++ src/main/java/com/github/ankurpapneja4/Calculator.java
@@ -0,0 +1,8 @@
+package com.github.ankurpapneja4;
+
+public class Calculator {
+
+    public static int sum(int x, int y){
+        return x + y;
+    }
+}
